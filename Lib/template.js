c3f629ef87c5aedb353d4d0541056ff6e20b8200
MizunashiAkari <mzns_utopia@outlook.com>
2018-11-04 20:10:17 +0800

    Add files via upload

---
+++ Lib/template.js
@@ -0,0 +1,29 @@
+var gl;
+var points;
+
+var program;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById("gl-canvas");
+    
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {alert("WebGL isn't available");}
+
+    //  Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    
+    //  Load shaders and initialize attribute buffers
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+	
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+
+function render(vColor, vPosition, func, ps, pe) {
+	gl.enableVertexAttribArray(vColor);
+	gl.enableVertexAttribArray(vPosition);
+	gl.drawArrays(func, ps, pe);
+}
